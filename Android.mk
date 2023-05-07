e64974cf0756c55b038b8babc734b65511a0175f
Albert Tang <tangalbert919@yahoo.com>
2023-05-07 17:19:25 +0000

    Revert "pal: Use ListenSoundModelLib.h from header library"
    This reverts commit 58e04b0.
    
    Change-Id: I29cde41c02bf0cd73fe020c69ed7fcca81246857

--- Android.mk
+++ Android.mk
@@ -142,7 +142,6 @@ LOCAL_HEADER_LIBRARIES := \
     libcapiv2_headers \
     libagm_headers \
     libacdb_headers \
-    liblisten_headers \
     libarosal_headers \
     libvui_dmgr_headers \
     libaudiofeaturestats_headers \
