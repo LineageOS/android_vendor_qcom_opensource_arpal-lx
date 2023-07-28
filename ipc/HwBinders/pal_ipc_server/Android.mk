804e6e5341067b2210991c8a60b4da5c04b07979
Bruno Martins <bgcngm@gmail.com>
2023-07-28 13:03:17 +0100

    ipc: Update own paths
    Change-Id: Ie88c9f3aa058a00269322dd510ec970eb4738f9e

--- ipc/HwBinders/pal_ipc_server/Android.mk
+++ ipc/HwBinders/pal_ipc_server/Android.mk
@@ -12,7 +12,7 @@ LOCAL_SRC_FILES := \
     src/pal_server_wrapper.cpp
 
 LOCAL_C_INCLUDES := \
-    $(TOP)/vendor/qcom/opensource/pal/utils/inc
+    $(call project-path-for,qcom-audio)/pal/utils/inc
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/inc
 
