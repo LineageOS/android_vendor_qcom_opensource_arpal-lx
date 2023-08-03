a1504a0f3faed1e005dc2c74edba58d1a0af3fc7
Lakshmi Prahitha Pendam <quic_pendam@quicinc.com>
2023-08-03 20:13:51 +0530

    pal: update number of channels  in resourcemanager for proxy When we get device switch back to back without setparameters the value of channels is 0 because of which the channel number in resource manager is not getting updated. Hence, updating the number of channels and reset is done on device close when no device is connected.
    Change-Id: I54031d57587df4b8b1deed3a9e9e09f3880c5833

--- device/src/Device.cpp
+++ device/src/Device.cpp
@@ -493,6 +493,8 @@ int Device::close()
            disableDevice(audioRoute, mSndDeviceName);
            mCurrentPriority = MIN_USECASE_PRIORITY;
            deviceStartStopCount = 0;
+           if(rm->num_proxy_channels != 0)
+               rm->num_proxy_channels = 0;
        }
     }
     PAL_INFO(LOG_TAG, "Exit. deviceCount %d for device id %d (%s), exit status %d", deviceCount,
