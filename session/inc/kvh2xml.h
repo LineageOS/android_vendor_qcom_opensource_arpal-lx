e2479feb7da0545cb34514fe3914f57dc400ead8
Bruno Martins <bgcngm@gmail.com>
2023-09-06 14:30:38 +0100

    pal: Complement kvh2xml header
    Aligned with usecaseKvManager.xml from kalama and complemented with
    more values referenced from: https://dumps.tadiphone.dev/dumps/xiaomi/mivendor/-/blob/missi_phone_eea-user-13-TKQ1.221114.001-V14.0.1.0.TMAEUXM-release-keys/vendor/etc/kvh2xml.xml
    
    Change-Id: I2ea093832741901bc6409b7387d4be19611dc432
    (cherry picked from commit d22432bd423585e24cca23d6a1589bc575fc42ee)

--- session/inc/kvh2xml.h
+++ session/inc/kvh2xml.h
@@ -50,16 +50,36 @@ enum AllKeyIds{
 	DEVICEPP_RX = 0xAC000000,    /**< @h2xmle_name{DevicePP_Rx} */
 	DEVICEPP_TX = 0xAD000000,    /**< @h2xmle_name{DevicePP_Tx} */
 	MEDIAFMTID = 0xae000000,
+	STREAMPP_RX = 0xaf000000,
+	STREAMPP_TX = 0xb0000000,
 	STREAMTX = 0xb1000000,
 	EQUALIZER_SWITCH = 0xb2000000,
-	VIRTUALIZER_SWITCH = 0xb9000000,
-	REVERB_SWITCH = 0xb8000000,
+	VSID = 0xb3000000,
+	BTPROFILE = 0xb4000000,
+	BTFORMAT = 0xb5000000,
 	PBE_SWITCH = 0xb6000000,
 	BASS_BOOST_SWITCH = 0xb7000000,
+	REVERB_SWITCH = 0xb8000000,
+	VIRTUALIZER_SWITCH = 0xb9000000,
+	SW_SIDETONE = 0xba000000,
 	STREAM_SLOWTALK = 0xbb000000,
 	STREAM_CONFIG = 0xbc000000,
 	STREAM_MUXDEMUX = 0xbd000000,
-	TAG_KEY_SLOT_MASK = 0xe0000000
+	SPKRPROTDEVMAP = 0xbe000000,
+	SPKRPROTVIMAP = 0xbf000000,
+	RAS_SWITCH = 0xd0000000,
+	VOIP_SAMPLE_RATE = 0xd100000,
+	PROXYTXTYPE = 0xd1000000,
+	GAIN = 0xd2000000,
+	STREAM = 0xd3000000,
+	STREAMCHANNELS = 0xd4000000,
+	ICL = 0xd5000000,
+	DEVICETX_EXT = 0xd7000000,
+	DATALOGGING = 0xd8000000,
+	DTMF_SWITCH = 0xde000000,
+	DTMF_GEN_TONE = 0xdf000000,
+	TAG_KEY_SLOT_MASK = 0xe0000000,
+	DUTYCYCLE = 0xe1000000
 };
 
 /**
@@ -67,19 +87,41 @@ enum AllKeyIds{
 	@h2xmlk_description {Type of Rx Stream}
 */
 enum Key_StreamRX {
-	PCM_LL_PLAYBACK = 0xA1000001, /**< @h2xmle_name {PCM_LL_Playback}*/
-	PCM_RECORD = 0xA1000002,      /**< @h2xmle_name {PCM_Record}*/
-	PCM_LOOPBACK = 0xA1000003,    /**< @h2xmle_name {PCM_Loopback}*/
-	VOICE_UI = 0xA1000004,        /**< @h2xmle_name {Voice_UI}*/
-	VOIP_RX_PLAYBACK = 0xA1000005,/**< @h2xmle_name {Voip_Rx}*/
-	VOIP_TX_RECORD = 0xA1000006,   /**< @h2xmle_name {Voip_Tx}*/
-	VOICE_UI_EC_REF_PATH = 0xA1000007,/**< @h2xmle_name {Voice_UI_EC_Ref_Path}*/
-	VOIP_TX_EC_REF_PATH = 0xA1000008,   /**< @h2xmle_name {Voip_Tx_EC_Ref_Path}*/
-	PCM_RECORD_EC_REF_PATH = 0xA1000009,/**< @h2xmle_name {PCM_Record_EC_Ref_Path}*/
+	PCM_DEEP_BUFFER = 0xa1000001,
+	PCM_RX_LOOPBACK = 0xa1000003,
+	VOIP_RX_PLAYBACK = 0xa1000005,
 	COMPRESSED_OFFLOAD_PLAYBACK = 0xa100000a,
+	HFP_RX_PLAYBACK = 0xa100000c,
+	PCM_LL_PLAYBACK = 0xa100000e,
+	PCM_OFFLOAD_PLAYBACK = 0xa100000f,
+	VOICE_CALL_RX = 0xa1000010,
+	PCM_ULL_PLAYBACK = 0xa1000011,
+	PCM_PROXY_PLAYBACK = 0xa1000012,
+	INCALL_MUSIC = 0xa1000013,
+	GENERIC_PLAYBACK = 0xa1000014,
 	HAPTICS_PLAYBACK = 0xa1000015,
+	RAW_PLAYBACK = 0xa1000019,
 };
 
+/**
+	@h2xmlk_key {STREAMTX}
+	@h2xmlk_description {Type of Tx Stream}
+*/
+enum Key_StreamTX {
+	PCM_RECORD = 0xb1000001,
+	VOICE_UI = 0xb1000003,
+	VOIP_TX_RECORD = 0xb1000004,
+	HFP_RX_CAPTURE = 0xb1000005,
+	VOICE_CALL_TX = 0xb1000007,
+	RAW_RECORD = 0xb1000009,
+	PCM_ULL_RECORD = 0xb100000a,
+	PCM_PROXY_RECORD = 0xb100000b,
+	INCALL_RECORD = 0xb100000c,
+	ACD = 0xb100000d,
+	SENSOR_PCM_DATA = 0xb100000e,
+	VOICE_RECOGNITION_RECORD = 0xb1000011,
+	COMPRESS_CAPTURE = 0xb1000012,
+};
 
 /**
 	@h2xmlk_key {INSTANCE}
@@ -89,6 +131,11 @@ enum Key_Instance {
 	INSTANCE_1 = 1, /**< @h2xmle_name {Instance_1}*/
 	INSTANCE_2 = 2, /**< @h2xmle_name {Instance_2}*/
 	INSTANCE_3 = 3, /**< @h2xmle_name {Instance_3}*/
+	INSTANCE_4 = 4,
+	INSTANCE_5 = 5,
+	INSTANCE_6 = 6,
+	INSTANCE_7 = 7,
+	INSTANCE_8 = 8,
 };
 
 /**
@@ -96,17 +143,35 @@ enum Key_Instance {
 	@h2xmlk_description {Rx Device}
 */
 enum Key_DeviceRX {
-	SPEAKER = 0xA2000001, /**< @h2xmle_name {Speaker}*/
+	SPEAKER = 0xa2000001,
+	HEADPHONES = 0xa2000002,
+	BT_RX = 0xa2000003,
 	HANDSET = 0xa2000004,
+	USB_RX = 0xa2000005,
+	HDMI_RX = 0xa2000006,
+	PROXY_RX = 0xa2000007,
+	PROXY_RX_VOICE = 0xa2000008,
 	HAPTICS_DEVICE = 0xa2000009,
+	ULTRASOUND_RX = 0xa200000a,
+	ULTRASOUND_RX_DEDICATED = 0xa200000b,
 };
 /**
 	@h2xmlk_key {DEVICETX}
 	@h2xmlk_description {Tx Device}
 */
 enum Key_DeviceTX {
-	HANDSETMIC = 0xA3000004, /**< @h2xmle_name {HandsetMic}*/
-	HANDSETMIC_VA = 0xa3000006
+	SPEAKER_MIC = 0xa3000001,
+	BT_TX = 0xa3000002,
+	HEADPHONE_MIC = 0xa3000003,
+	HANDSETMIC = 0xa3000004,
+	USB_TX = 0xa3000005,
+	HANDSETMIC_VA = 0xa3000006,
+	HEADSETMIC_VA = 0xa3000007,
+	PROXY_TX = 0xa3000008,
+	VI_TX = 0xa3000009,
+	FM_TX = 0xa300000a,
+	ULTRASOUND_TX = 0xa300000b,
+	ECHO_REF_TX = 0xa300000d,
 };
 
 /**
@@ -114,20 +179,45 @@ enum Key_DeviceTX {
 	@h2xmlk_description {Rx Device Post/Pre Processing Chain}
 */
 enum Key_DevicePP_RX {
-	DEVICEPP_RX_DEFAULT = 0xAC000001, /**< @h2xmle_name {Audio_LL_Default_PP} @h2xmlk_description {Low Latency Default Playback}*/
+	DEVICEPP_RX_DEFAULT = 0xac000001,
 	DEVICEPP_RX_AUDIO_MBDRC = 0xac000002,
+	DEVICEPP_RX_VOIP_MBDRC = 0xac000003,
+	DEVICEPP_RX_HFPSINK = 0xac000004,
+	DEVICEPP_RX_VOICE_DEFAULT = 0xac000005,
+	DEVICEPP_RX_ULTRASOUND_GENERATOR = 0xac000006,
+	DEVICEPP_RX_VOICE_RVE = 0xac000007,
+	DEVICEPP_RX_VOICE_NN_NS = 0xac000009,
+	DEVICEPP_RX_AUDIO_MSPP = 0xac00000b,
 };
+
 /**
 	@h2xmlk_key {DEVICEPP_TX}
 	@h2xmlk_description {Tx Device Post/Pre Processing Chain}
 */
 enum Key_DevicePP_TX {
-	DEVICEPP_TX_FLUENCE_FFECNS        = 0xAD000001, /**< @h2xmle_name {Voice_Fluence_FFECNS} @h2xmlk_description {Used in Voice UI use-cases}*/
-	DEVICEPP_TX_AUDIO_FLUENCE_SMECNS  = 0xAD000002, /**< @h2xmle_name {Audio_Fluence_SMECNS} @h2xmlk_description {Single Mic ECNS }*/
-	DEVICEPP_TX_AUDIO_FLUENCE_ENDFIRE = 0xAD000003, /**< @h2xmle_name {Audio_Fluence_Endfire} @h2xmlk_description {EndFire_ECNS - Typically used for dual mic capture scenarios}*/
-	DEVICEPP_TX_AUDIO_FLUENCE_PRO     = 0xAD000004, /**< @h2xmle_name {Audio_Fluence_Pro} @h2xmlk_description {Multi MIC scenarios ; at least 3 or more Micss}*/
+	DEVICEPP_TX_FLUENCE_FFECNS = 0xad000001,
+	DEVICEPP_TX_AUDIO_FLUENCE_SMECNS = 0xad000002,
+	DEVICEPP_TX_AUDIO_FLUENCE_ENDFIRE = 0xad000003,
+	DEVICEPP_TX_AUDIO_FLUENCE_PRO = 0xad000004,
+	DEVICEPP_TX_VOIP_FLUENCE_PRO = 0xad000005,
+	DEVICEPP_TX_HFP_SINK_FLUENCE_SMECNS = 0xad000006,
+	DEVICEPP_TX_VOIP_FLUENCE_SMECNS = 0xad000007,
+	DEVICEPP_TX_VOICE_FLUENCE_SMECNS = 0xad000008,
+	DEVICEPP_TX_VOICE_FLUENCE_ENDFIRE = 0xad000009,
+	DEVICEPP_TX_VOICE_FLUENCE_PRO = 0xad00000a,
+	DEVICEPP_TX_RAW_LPI = 0xad00000c,
+	DEVICEPP_TX_VOIP_FLUENCE_ENDFIRE = 0xad00000d,
+	DEVICEPP_TX_RAW_NLPI = 0xad00000e,
+	DEVICEPP_TX_VOICE_FLUENCE_NN_SM = 0xad00000f,
+	DEVICEPP_TX_VOIP_FLUENCE_NN_SM = 0xad000010,
+	DEVICEPP_TX_FLUENCE_FFEC = 0xad000012,
+	DEVICEPP_TX_VOICE_FLUENCE_ENDFIRE_RVE = 0xad000013,
+	DEVICEPP_TX_VOICE_RECOGNITION = 0xad000017,
+	DEVICEPP_TX_AAD = 0xad000019,
+	DEVICEPP_TX_FLUENCE_FFNS_AAD = 0xad00001a,
+	DEVICEPP_TX_RAW_LPI_AAD = 0xad00001b,
+	DEVICEPP_TX_AUDIO_RECORD_ENQORE = 0xad000023,
 };
-
 
 enum Key_StreamConfig {
 	STREAM_CFG_VUI_SVA = 0xbc000001,
@@ -155,6 +245,30 @@ enum Key_Volume {
 	LEVEL_14 = 14, /**< @h2xmle_name {Level_14}*/
 	LEVEL_15 = 15, /**< @h2xmle_name {Level_15}*/
 };
+
+/**
+	@h2xmlk_key {GAIN}
+	@h2xmlk_description {Gain}
+*/
+enum Key_Gain {
+	GAIN_0 = 0, /**< @h2xmle_name {Gain_0}*/
+	GAIN_1 = 1, /**< @h2xmle_name {Gain_1}*/
+	GAIN_2 = 2, /**< @h2xmle_name {Gain_2}*/
+	GAIN_3 = 3, /**< @h2xmle_name {Gain_3}*/
+	GAIN_4 = 4, /**< @h2xmle_name {Gain_4}*/
+	GAIN_5 = 5, /**< @h2xmle_name {Gain_5}*/
+	GAIN_6 = 6, /**< @h2xmle_name {Gain_6}*/
+	GAIN_7 = 7, /**< @h2xmle_name {Gain_7}*/
+	GAIN_8 = 8, /**< @h2xmle_name {Gain_8}*/
+	GAIN_9 = 9, /**< @h2xmle_name {Gain_9}*/
+	GAIN_10 = 10, /**< @h2xmle_name {Gain_10}*/
+	GAIN_11 = 11, /**< @h2xmle_name {Gain_11}*/
+	GAIN_12 = 12, /**< @h2xmle_name {Gain_12}*/
+	GAIN_13 = 13, /**< @h2xmle_name {Gain_13}*/
+	GAIN_14 = 14, /**< @h2xmle_name {Gain_14}*/
+	GAIN_15 = 15, /**< @h2xmle_name {Gain_15}*/
+};
+
 /**
 	@h2xmlk_key {SAMPLINGRATE}
 	@h2xmlk_sampleRate
@@ -163,11 +277,14 @@ enum Key_Volume {
 enum Key_SamplingRate {
 	SAMPLINGRATE_8K = 8000,   /**< @h2xmle_sampleRate{8000} @h2xmle_name {SR_8K}*/
 	SAMPLINGRATE_16K = 16000, /**< @h2xmle_sampleRate{16000} @h2xmle_name {SR_16K}*/
+	SAMPLINGRATE_22K = 22050, /**< @h2xmle_sampleRate{22050} @h2xmle_name {SR_22K}*/
+	SAMPLINGRATE_24K = 24000, /**< @h2xmle_sampleRate{24000} @h2xmle_name {SR_24K}*/
 	SAMPLINGRATE_32K = 32000, /**< @h2xmle_sampleRate{32000} @h2xmle_name {SR_32K}*/
 	SAMPLINGRATE_44K = 44100, /**< @h2xmle_sampleRate{44100} @h2xmle_name {SR_44.1K}*/
 	SAMPLINGRATE_48K = 48000, /**< @h2xmle_sampleRate{48000} @h2xmle_name {SR_48K}*/
 	SAMPLINGRATE_96K = 96000, /**< @h2xmle_sampleRate{96000} @h2xmle_name {SR_96K}*/
 	SAMPLINGRATE_192K = 192000, /**< @h2xmle_sampleRate{192000} @h2xmle_name {SR_192K}*/
+	SAMPLINGRATE_352K = 352800, /**< @h2xmle_sampleRate{352800} @h2xmle_name {SR_352K}*/
 	SAMPLINGRATE_384K = 384000, /**< @h2xmle_sampleRate{384000} @h2xmle_name {SR_384K}*/
 };
 /**
@@ -222,6 +339,15 @@ enum Key_ECNS {
 };
 
 /**
+	@h2xmlk_key {DTMF}
+	@h2xmlk_description {DMTF_SWITCH}
+*/
+enum Key_Dtmf {
+	DISABLE = 0,
+	ENABLE = 1,
+};
+
+/**
 	@h2xmlk_gkeys
 	@h2xmlk_description {Graph Keys}
 */
@@ -232,7 +358,17 @@ enum Graph_Keys {
 	gk_DevicePP_RX = DEVICEPP_RX,
 	gk_DevicePP_TX = DEVICEPP_TX,
 	gk_Instance = INSTANCE,
+	gk_StreamPP_RX = STREAMPP_RX,
+	gk_StreamPP_TX = STREAMPP_TX,
 	gk_StreamTX = STREAMTX,
+	gk_VSID = VSID,
+	gk_BtProfile = BTPROFILE,
+	gk_BtFormat = BTFORMAT,
+	gk_SW_Sidetone = SW_SIDETONE,
+	gk_Stream_Config = STREAM_CONFIG,
+	gk_ProxyTxType = PROXYTXTYPE,
+	gk_Stream = STREAM,
+	gk_DeviceTX_EXT = DEVICETX_EXT,
 };
 /**
 	@h2xmlk_ckeys
@@ -240,6 +376,13 @@ enum Graph_Keys {
 */
 enum Cal_Keys {
 	ck_volume = VOLUME,
+	ck_channels = CHANNELS,
+	ck_sp_dev_map = SPKRPROTDEVMAP,
+	ck_ras_switch = RAS_SWITCH,
+	ck_sp_vi_map = SPKRPROTVIMAP,
+	ck_gain = GAIN,
+	ck_stream_channels = STREAMCHANNELS,
+	ck_voip_sample_rate = VOIP_SAMPLE_RATE,
 };
 
 #define DEVICE_HW_ENDPOINT_RX        0xC0000004
@@ -389,6 +532,8 @@ enum TAG_DEVICE_MUX_Keys {
 	tk1_SlotMask= TAG_KEY_SLOT_MASK,
 };
 
+#define TAG_MODULE_MSPP  0xc0000043
+
 /**
 	@h2xmlk_modTagList
 */
@@ -403,6 +548,22 @@ enum TAGS_DEFINITIONS {
         STREAM_PCM_DECODER          = 0xC000000E, /**< @h2xmle_name {"stream_pcm_decoder"} */
         STREAM_PCM_ENCODER          = 0xC000000F, /**< @h2xmle_name {"stream_pcm_encoder"} */
         STREAM_PCM_CONVERTER        = 0xC0000010, /**< @h2xmle_name {"stream_pcm_converter"} */
+        STREAM_SPR                  = 0xc0000013,
+        BT_ENCODER                  = 0xc0000020,
+        COP_PACKETIZER_V0           = 0xc0000021,
+        RATE_ADAPTER_MODULE         = 0xc0000022,
+        BT_PCM_CONVERTER            = 0xc0000023,
+        BT_DECODER                  = 0xc0000024,
+        MODULE_VI                   = 0xc0000028,
+        MODULE_SP                   = 0xc0000029,
+        MODULE_GAPLESS              = 0xc000002a,
+        WR_SHMEM_ENDPOINT           = 0xc000002c,
+        RD_SHMEM_ENDPOINT           = 0xc000002e,
+        COP_PACKETIZER_V2           = 0xc000002f,
+        COP_DEPACKETIZER_V2         = 0xc0000030,
+        CONTEXT_DETECTION_ENGINE    = 0xc0000031,
+        ULTRASOUND_DETECTION_MODULE = 0xc0000032,
+        DEVICE_POP_SUPPRESSOR       = 0xc000003a,
 };
 typedef enum TAGS_DEFINITIONS TAGS_DEFINITIONS;
 
