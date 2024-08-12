2e928e926cbf338d7145d9cb45b8502a5e308eb1
Arian <arian.kulmer@web.de>
2024-08-12 14:20:53 +0200

    Disable 32 bit variants of audio libraries for 64 bit audio targets
    This allows to avoid having to provide the 32bit variants of proprietary audio modules, like libar-pal
    
    Change-Id: I178d9a3e25462a380740903b2b693142bdb64e0b

--- ipc/HwBinders/pal_ipc_server/Android.mk
+++ ipc/HwBinders/pal_ipc_server/Android.mk
@@ -4,6 +4,10 @@ ifneq ($(QCPATH),)
 
 include $(CLEAR_VARS)
 
+ifeq ($(SOONG_CONFIG_android_hardware_audio_run_64bit), true)
+LOCAL_MULTILIB := 64
+endif
+
 LOCAL_MODULE := vendor.qti.hardware.pal@1.0-impl
 LOCAL_MODULE_OWNER := qti
 LOCAL_VENDOR_MODULE := true
