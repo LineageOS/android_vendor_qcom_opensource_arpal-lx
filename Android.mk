2312a24373f0a751339555517903f5bc048d4c05
Michael Bestas <mkbestas@lineageos.org>
2025-01-24 10:10:03 +0200

    Reapply "pal: Use ListenSoundModelLib.h from header library"
    This reverts commit 1e633bcd21c321fe686101a647e4689ed41365cd.
    
    Change-Id: I69ed54f7159f376b9b1d3c02e41cbacc2b453358

--- Android.mk
+++ Android.mk
@@ -133,7 +133,8 @@ LOCAL_HEADER_LIBRARIES := \
     libcapiv2_headers \
     libagm_headers \
     libacdb_headers \
-    libarpal_headers
+    libarpal_headers \
+    liblisten_headers
 
 LOCAL_SHARED_LIBRARIES := \
     libar-gsl\
