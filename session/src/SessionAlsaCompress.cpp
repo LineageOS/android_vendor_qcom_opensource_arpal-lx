80dcf7a88f7f998471faa0ec00b80beb0caa8c46
Arian <arian.kulmer@web.de>
2025-01-31 00:31:31 +0100

    WIP add support for awinic speaker
    Change-Id: I9e6b6936650c3aaa5a862b4c17037ac215317c1b

--- session/src/SessionAlsaCompress.cpp
+++ session/src/SessionAlsaCompress.cpp
@@ -1326,6 +1326,21 @@ int SessionAlsaCompress::start(Stream * s)
                         PAL_ERR(LOG_TAG, "setMixerParameter failed");
                         goto exit;
                     }
+
+                    // HERE AWINIC CALIB
+                    {
+                        struct pal_device dattr;
+                        dattr.id = PAL_DEVICE_OUT_SPEAKER;
+                        std::shared_ptr<Device> dev = nullptr;
+                        dev = Device::getInstance(&dattr , rm);
+                        if (dev) {
+                            PAL_DBG(LOG_TAG, "Got Speaker instance");
+                            dev->setParameter(PAL_SP_MODE_AW_CAL, nullptr);
+                        }
+                        else {
+                            PAL_DBG(LOG_TAG, "Unable to get speaker instance");
+                        }
+                    }
                 }
 
                 if (!status && isMixerEventCbRegd && !isPauseRegistrationDone) {
