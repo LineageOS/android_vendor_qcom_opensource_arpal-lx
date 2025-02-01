c3c7f96a704cf0aef55b1ce54afea99456176a53
Bruno Martins <bgcngm@gmail.com>
2025-02-01 11:13:51 +0000

    pal: Allow opting-out for libar-pal and pal_ipc_server
    Change-Id: I256de424dae47287ddd4f0974ecaed39cce1f133

--- ipc/HwBinders/pal_ipc_server/Android.mk
+++ ipc/HwBinders/pal_ipc_server/Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(TARGET_PROVIDES_LIBAR_PAL),true)
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -38,4 +39,4 @@ LOCAL_HEADER_LIBRARIES := \
     libacdb_headers
 
 include $(BUILD_SHARED_LIBRARY)
-
+endif
