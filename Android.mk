78f06c93992afff0ad21d7dff2fcaf65dd04d7f8
Bruno Martins <bgcngm@gmail.com>
2025-02-01 11:13:51 +0000

    pal: Allow opting-out for libar-pal and pal_ipc_server
    Change-Id: I256de424dae47287ddd4f0974ecaed39cce1f133

--- Android.mk
+++ Android.mk
@@ -25,6 +25,7 @@ LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_HEADER_LIBRARY)
 
+ifneq ($(TARGET_PROVIDES_AUDIO_HAL),true)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE        := libar-pal
@@ -182,6 +183,7 @@ LOCAL_SHARED_LIBRARIES += libtinyalsa libtinycompress
 endif
 
 include $(BUILD_SHARED_LIBRARY)
+endif
 
 #-------------------------------------------
 #            Build CHARGER_LISTENER LIB
