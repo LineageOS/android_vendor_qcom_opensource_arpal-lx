c69408f6cda550cf47451f6b0eedb16839845c3d
pjgowtham <pjgowtham@gmail.com>
2025-02-24 18:44:37 +0530

    pal: SpeakerTfa98xx: Send calibrated impedance to DSP
    Change-Id: Ieb7b887de1c09b8518ab0c4f9bb4000bedb4ceb1

--- device/src/SpeakerProtection.cpp
+++ device/src/SpeakerProtection.cpp
@@ -2152,6 +2152,10 @@ SpeakerProtection::SpeakerProtection(struct pal_device *device,
         PAL_ERR(LOG_TAG,"hw mixer error %d", status);
     }
 
+    if (SpeakerTfa98xx::isTfaDevicePresent(hwMixer)) {
+        tfa98xx = std::make_unique<SpeakerTfa98xx>();
+    }
+
     fp = fopen(PAL_SP_TEMP_PATH, "rb");
     if (fp) {
         PAL_DBG(LOG_TAG, "Cal File exists. Reading from it");
@@ -3636,8 +3640,12 @@ int32_t SpeakerProtection::spkrProtProcessingMode(bool flag)
         }
 
         payloadSize = 0;
-        builder->payloadSPConfig(&payload, &payloadSize, miid,
-                PARAM_ID_SP_OP_MODE,(void *)&spModeConfg);
+        if (tfa98xx) {
+            tfa98xx->payloadSPConfig(&payload, &payloadSize, miid);
+        } else {
+            builder->payloadSPConfig(&payload, &payloadSize, miid,
+                    PARAM_ID_SP_OP_MODE, (void *)&spModeConfg);
+        }
         if (payloadSize) {
             if (customPayload) {
                 free (customPayload);
