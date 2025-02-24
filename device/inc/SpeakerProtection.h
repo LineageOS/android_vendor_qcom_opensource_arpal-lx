109ed23c205b6d08ecc74c93fd8153a07b5d349e
pjgowtham <pjgowtham@gmail.com>
2025-02-24 18:44:37 +0530

    pal: SpeakerTfa98xx: Send calibrated impedance to DSP
    Change-Id: Ieb7b887de1c09b8518ab0c4f9bb4000bedb4ceb1

--- device/inc/SpeakerProtection.h
+++ device/inc/SpeakerProtection.h
@@ -41,6 +41,7 @@
 #include<vector>
 #include "apm_api.h"
 #include "ResourceManager.h"
+#include "SpeakerTfa98xx.h"
 
 class Device;
 
@@ -115,6 +116,7 @@ protected :
     struct spDeviceInfo spDevInfo;
     void *viCustomPayload;
     size_t viCustomPayloadSize;
+    std::unique_ptr<SpeakerTfa98xx> tfa98xx;
 
 private :
     static bool isSharedBE;
