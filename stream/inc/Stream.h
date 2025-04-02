bb044c0a72588781175a746bb08656201d9fc5dd
Arian <arian.kulmer@web.de>
2025-04-02 20:20:44 +0200

    Implement speaker protection profiles
    Change-Id: I9f226673e82f69e8e2c2e926327ffcde7b16f94e

--- stream/inc/Stream.h
+++ stream/inc/Stream.h
@@ -114,6 +114,7 @@ typedef enum {
 #define DEVICEPP_MUTE 45
 #define DEVICEPP_UNMUTE 46
 #define HANDSET_PROT_ENABLE 47
+#define SPKR_PROT_PROFILE 82
 
 /* This sleep is added to give time to kernel and
  * spf to recover from SSR so that audio-hal will
