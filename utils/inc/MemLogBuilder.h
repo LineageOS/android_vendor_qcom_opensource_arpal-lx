4610c4c424cb986c172f7b381518814f55eed569
Cosmin Tanislav <demonsingur@gmail.com>
2025-04-30 02:03:31 +0300

    pal: disable mem logger support
    This depends on libarmemlog which is properietary.
    
    Change-Id: I453cbea4477f941dd08df6afffa037454e941638

--- utils/inc/MemLogBuilder.h
+++ utils/inc/MemLogBuilder.h
@@ -6,17 +6,33 @@
 #ifndef MEMLOG_BUILDER_H
 #define MEMLOG_BUILDER_H
 
+#ifndef PAL_MEMLOG_UNSUPPORTED
 #include "mem_logger.h"
 #include "pal_state_queue.h"
 #include "kpi_queue.h"
+#else
+enum pal_state_queue_state {
+    PAL_STATE_OPENED,
+    PAL_STATE_STARTED,
+    PAL_STATE_PAUSED,
+    PAL_STATE_SUSPENDED,
+    PAL_STATE_STOPPED,
+    PAL_STATE_CLOSED,
+};
+#endif
 #include "ResourceManager.h"
 #include "Stream.h"
 #include <inttypes.h>
 
+#ifndef PAL_MEMLOG_UNSUPPORTED
 int palStateQueueBuilder(pal_state_queue &que, Stream *s, pal_state_queue_state state, int32_t error);
 int palStateEnqueue(Stream *s, pal_state_queue_state state, int32_t error);
 int palStateEnqueue(Stream *s, pal_state_queue_state state, int32_t error, union pal_mlog_str_info str_info);
 pal_mlog_acdstr_info palStateACDStreamBuilder(Stream *s);
 void kpiEnqueue(const char name[], bool isEnter);
+#else
+static inline int palStateEnqueue(Stream *s, pal_state_queue_state state, int32_t error) {return 0;}
+static inline void kpiEnqueue(const char name[], bool isEnter) {return;}
+#endif
 
 #endif
