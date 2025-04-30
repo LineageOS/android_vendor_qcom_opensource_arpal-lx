e978f6196aa67bc0011000d449942aa06c2826b5
Cosmin Tanislav <demonsingur@gmail.com>
2025-04-30 04:48:11 +0300

    pal: fix constructor reordering
    Co-authored-by: dianlujitao <dianlujitao@lineageos.org>
    Change-Id: Id95be2e273860befdac1d55226a533f0cc99cf70

--- utils/src/VoiceUIPlatformInfo.cpp
+++ utils/src/VoiceUIPlatformInfo.cpp
@@ -213,9 +213,9 @@ VUIStreamConfig::VUIStreamConfig() :
     pre_roll_duration_(0),
     supported_first_stage_engine_count_(1),
     enable_intra_concurrent_detection_(false),
-    curr_child_(nullptr),
     lpi_enable_(true),
     batch_size_in_ms_(0),
+    curr_child_(nullptr),
     enable_amd_(false)
 {
     ext_det_prop_list_.clear();
