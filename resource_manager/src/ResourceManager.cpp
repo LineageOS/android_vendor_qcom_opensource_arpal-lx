3f1e41c02c8fd88d65e6d49b3c98780e10ec00ff
Cosmin Tanislav <demonsingur@gmail.com>
2025-07-26 16:36:55 +0300

    pal: fix activeStreams check
    getSharedBEActiveStreamDevs() is supposed to retrieve Devices that
    share the same backend but are not the same. Fix the check.
    
    Change-Id: I684301c2844d99b1c9597bc8dd8930d32452671c

--- resource_manager/src/ResourceManager.cpp
+++ resource_manager/src/ResourceManager.cpp
@@ -6623,7 +6623,7 @@ void ResourceManager::getSharedBEActiveStreamDevs(std::vector <std::tuple<Stream
                     (*it)->getAssociatedDevices(devices);
                     typename std::vector<std::shared_ptr<Device>>::iterator result =
                              std::find(devices.begin(), devices.end(), dev);
-                    if ((result != devices.end()) && (i == dev_id))
+                    if ((result != devices.end()) && (i != (*result)->id))
                         activeStreams.push_back(*it);
                 }
                 PAL_DBG(LOG_TAG, "got dev %d active streams on dev is %zu", i, activeStreams.size() );
